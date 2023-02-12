fe8e98a9a8967d5c13beafb0880c671123f25e55
Panagiotis Bellias <panagiotis.bellias@eurodyn.com>
2023-02-12 17:11:30 +0200

    Create basic spring boot application

---
+++ src/test/java/com/example/spring/boot/tutorial/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.boot.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
