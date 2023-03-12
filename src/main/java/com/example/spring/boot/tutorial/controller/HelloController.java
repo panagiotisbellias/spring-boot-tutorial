b5eac16b25fea8b39328608557d5179a8f005dbe
Panagiotis Bellias <panagiotis.bellias@eurodyn.com>
2023-03-12 16:55:46 +0200

    Fix packaging

--- src/main/java/com/example/spring/boot/tutorial/controller/HelloController.java
+++ src/main/java/com/example/spring/boot/tutorial/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.spring.boot.tutorial;
+package com.example.spring.boot.tutorial.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
